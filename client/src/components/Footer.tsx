abe37a29340cac987b0e11f2823d09566a8e84f4
My Chiffon Nguyen <chiffonng136@gmail.com>
2025-11-18 22:21:13 -0800

    style(footer): structure and links

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,33 +1,37 @@
 function Footer() {
   return (
     <footer className="footer footer-center bg-base-300 text-base-content mt-auto p-4">
-      Copyright © 2025 SportConnect. Developed by{" "}
-      <a
-        href="https://www.linkedin.com/in/ep3100/"
-        rel="noopener noreferrer nofollow"
-        target="_blank"
-        className="link link-hover"
-      >
-        Emanuel Pimentel
-      </a>{" "}
-      and{" "}
-      <a
-        href="https://mychiffonn.com"
-        rel="noopener noreferrer nofollow"
-        target="_blank"
-        className="link link-hover"
-      >
-        My Chiffon Nguyen
-      </a>{" "}
-      with 🩵.
-      <a
-        href="https://github.com/mychiffonn/sport-connect"
-        rel="noopener noreferrer nofollow"
-        target="_blank"
-        className="link link-hover"
-      >
-        Source Code
-      </a>
+      <div className="flex flex-wrap items-center justify-center gap-2">
+        <span>
+          Copyright © 2025 SportConnect. Developed by{" "}
+          <a
+            href="https://www.linkedin.com/in/ep3100/"
+            rel="noopener noreferrer nofollow"
+            target="_blank"
+            className="link link-hover underline"
+          >
+            Emanuel Pimentel
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://mychiffonn.com"
+            rel="noopener noreferrer nofollow"
+            target="_blank"
+            className="link link-hover underline"
+          >
+            My Chiffon Nguyen
+          </a>{" "}
+          with 🩵.
+        </span>
+        <a
+          href="https://github.com/mychiffonn/sport-connect"
+          rel="noopener noreferrer nofollow"
+          target="_blank"
+          className="link link-hover underline"
+        >
+          Source Code
+        </a>
+      </div>
     </footer>
   )
 }
